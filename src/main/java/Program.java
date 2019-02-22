47250db3aa21e7a54c679d881a8aa07f97d9bcaa
zhumengyifang <wangjiayuzh@163.com>
2019-02-23 00:02:40 +0800

    集成Junit 单元测试

--- src/main/java/Program.java
+++
@@ -1,7 +0,0 @@
-import org.demo.EasySolution;
-
-public class Program {
-    public static void main(String[] args) {
-        EasySolution solution=new EasySolution();
-    }
-}
---
+++ src/test/java/EasySolutionTest.java
@@ -0,0 +1,11 @@
+import org.demo.EasySolution;
+
+import static org.junit.Assert.*;
+
+public class EasySolutionTest {
+
+    @org.junit.Test
+    public void isPalindrome() {
+        System.out.println("测试");
+    }
+}
