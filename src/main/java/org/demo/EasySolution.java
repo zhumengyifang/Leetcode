d0e420aab7a425e68400e924f5520837ef24adf0
jiayu <wangjiayuzh@163.com>
2019-02-28 19:25:32 +0800

    反转链表

--- src/main/java/org/demo/EasySolution.java
+++ src/main/java/org/demo/EasySolution.java
@@ -538,6 +538,25 @@ public class EasySolution {
             }
             return maj;
     }
+	
+	//反转链表
+	 public ListNode reverseList(ListNode head) {
+           if (head == null)
+                return head;
+
+            ListNode dummy = new ListNode(-1);
+            dummy.next = head;
+            ListNode prev = dummy.next;
+            ListNode pCur = prev.next;
+            while (pCur != null)
+            {
+                prev.next = pCur.next;
+                pCur.next = dummy.next;
+                dummy.next = pCur;
+                pCur = prev.next;
+            }
+            return dummy.next;
+    }
 
 }
 
